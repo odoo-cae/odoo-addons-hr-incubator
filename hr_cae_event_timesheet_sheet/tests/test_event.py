048b04e58be2e2235318b5a1c4691abc11ec7782
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-06-12 16:00:51 +0200

    [REF] hr_cae_event_timesheet_sheet: make tests unitary

--- hr_cae_event_timesheet_sheet/tests/test_event.py
+++ hr_cae_event_timesheet_sheet/tests/test_event.py
@@ -7,82 +7,88 @@ from odoo import tests
 
 
 class TestEvent(tests.common.TransactionCase):
-    def test_create_portal_user_from_contact(self):
+    def test_create_timesheet_line_for_event_admin_user(self):
         event = self.browse_ref("event.event_0")
         admin_user = self.browse_ref("base.user_admin")
-        partner_1 = self.browse_ref("base.res_partner_1")
-        partner_2 = self.browse_ref("base.res_partner_2")
-        partner_3 = self.browse_ref("base.res_partner_3")
+        account = self.browse_ref("analytic.analytic_rd_department")
+        project = self.browse_ref("project.project_project_2")
+
+        wizard = (
+            self.env["hr.cae.event.to.timesheet.wizard"]
+            .with_context({"active_id": event.id})
+            .create({"account_id": account.id, "project_id": project.id})
+        )
+
+        self.assertTrue(admin_user in wizard.user_ids)
+
+        wizard.create_account_analytic_line()
+
+        self.assertTrue(
+            self.env["account.analytic.line"].search(
+                [("user_id", "=", admin_user.id)]
+            )
+        )
+
+    def test_create_timesheet_line_for_event_partner(self):
+        event = self.browse_ref("event.event_0")
+        partner = self.browse_ref("base.res_partner_1")
+        account = self.browse_ref("analytic.analytic_rd_department")
+        project = self.browse_ref("project.project_project_2")
+
+        self.assertFalse(
+            self.env["res.users"].search([("login", "=", partner.email)])
+        )
+
+        partner_user = self.env["res.users"].create(
+            {
+                "name": partner.name,
+                "login": partner.email,
+                "email": partner.email,
+                "partner_id": partner.id,
+            }
+        )
+        self.env["event.registration"].create(
+            {
+                "event_id": event.id,
+                "name": partner.name,
+                "email": partner.email,
+                "partner_id": partner.id,
+                "state": "done",
+            }
+        )
+
+        wizard = (
+            self.env["hr.cae.event.to.timesheet.wizard"]
+            .with_context({"active_id": event.id})
+            .create({"account_id": account.id, "project_id": project.id})
+        )
+
+        self.assertTrue(partner_user in wizard.user_ids)
+
+        wizard.create_account_analytic_line()
+
+        self.assertTrue(
+            self.env["account.analytic.line"].search(
+                [("user_id", "=", partner_user.id)]
+            )
+        )
+
+    def test_create_timesheet_line_for_event_employee(self):
+        event = self.browse_ref("event.event_0")
         employee = self.browse_ref("hr.employee_al")
         account = self.browse_ref("analytic.analytic_rd_department")
         project = self.browse_ref("project.project_project_2")
 
         self.assertFalse(
-            self.env["res.users"].search([("login", "=", partner_1.email)])
-        )
-        self.assertFalse(
-            self.env["res.users"].search([("login", "=", partner_2.email)])
-        )
-        self.assertFalse(
-            self.env["res.users"].search([("login", "=", partner_3.email)])
-        )
-        self.assertFalse(
             self.env["res.users"].search([("login", "=", employee.work_email)])
         )
 
-        partner_1_user = self.env["res.users"].create(
-            {
-                "name": partner_1.name,
-                "login": partner_1.email,
-                "email": partner_1.email,
-                "partner_id": partner_1.id,
-            }
-        )
-        partner_2_user = self.env["res.users"].create(
-            {
-                "name": partner_2.name,
-                "login": partner_2.email,
-                "email": partner_2.email,
-            }
-        )
-        partner_3_user = self.env["res.users"].create(
-            {
-                "name": partner_3.name,
-                "login": partner_3.email,
-                "email": partner_3.email,
-            }
-        )
         employee_user = self.env["res.users"].create(
             {
                 "name": employee.name,
                 "login": employee.work_email,
                 "email": employee.work_email,
                 "employee_ids": [(6, 0, [employee.id])],
-            }
-        )
-        self.env["event.registration"].create(
-            {
-                "event_id": event.id,
-                "name": partner_1.name,
-                "email": partner_1.email,
-                "partner_id": partner_1.id,
-                "state": "done",
-            }
-        )
-        self.env["event.registration"].create(
-            {
-                "event_id": event.id,
-                "name": partner_2.name,
-                "email": partner_2.email,
-                "state": "done",
-            }
-        )
-        self.env["event.registration"].create(
-            {
-                "event_id": event.id,
-                "name": partner_3.name,
-                "email": partner_3.email,
-                "state": "open",  # tests that state 'done' is required
             }
         )
         self.env["event.registration"].create(
@@ -101,26 +107,90 @@ class TestEvent(tests.common.TransactionCase):
             .create({"account_id": account.id, "project_id": project.id})
         )
 
-        self.assertTrue(admin_user in wizard.user_ids)
-        self.assertTrue(partner_1_user in wizard.user_ids)
-        self.assertTrue(partner_2_user in wizard.user_ids)
-        self.assertFalse(partner_3_user in wizard.user_ids)
         self.assertTrue(employee_user in wizard.user_ids)
 
-        wizard.create_timesheet_sheets()
+        wizard.create_account_analytic_line()
 
-        self.assertTrue(
-            self.env["account.analytic.line"].search(
-                [("user_id", "=", partner_1_user.id)]
-            )
-        )
-        self.assertTrue(
-            self.env["account.analytic.line"].search(
-                [("user_id", "=", partner_2_user.id)]
-            )
-        )
         self.assertTrue(
             self.env["account.analytic.line"].search(
                 [("user_id", "=", employee_user.id)]
             )
         )
+
+    def test_create_timesheet_line_for_event_email(self):
+        event = self.browse_ref("event.event_0")
+        partner = self.browse_ref("base.res_partner_1")
+        account = self.browse_ref("analytic.analytic_rd_department")
+        project = self.browse_ref("project.project_project_2")
+
+        self.assertFalse(
+            self.env["res.users"].search([("login", "=", partner.email)])
+        )
+
+        partner_user = self.env["res.users"].create(
+            {
+                "name": partner.name,
+                "login": partner.email,
+                "email": partner.email,
+            }
+        )
+        self.env["event.registration"].create(
+            {
+                "event_id": event.id,
+                "name": partner.name,
+                "email": partner.email,
+                "state": "done",
+            }
+        )  # no partner or employee linked here, only email
+
+        wizard = (
+            self.env["hr.cae.event.to.timesheet.wizard"]
+            .with_context({"active_id": event.id})
+            .create({"account_id": account.id, "project_id": project.id})
+        )
+
+        self.assertTrue(partner_user in wizard.user_ids)
+
+        wizard.create_account_analytic_line()
+
+        self.assertTrue(
+            self.env["account.analytic.line"].search(
+                [("user_id", "=", partner_user.id)]
+            )
+        )
+
+    def test_dont_find_user_if_not_registration_done(self):
+        event = self.browse_ref("event.event_0")
+        partner = self.browse_ref("base.res_partner_1")
+        account = self.browse_ref("analytic.analytic_rd_department")
+        project = self.browse_ref("project.project_project_2")
+
+        self.assertFalse(
+            self.env["res.users"].search([("login", "=", partner.email)])
+        )
+
+        partner_user = self.env["res.users"].create(
+            {
+                "name": partner.name,
+                "login": partner.email,
+                "email": partner.email,
+                "partner_id": partner.id,
+            }
+        )
+        self.env["event.registration"].create(
+            {
+                "event_id": event.id,
+                "name": partner.name,
+                "email": partner.email,
+                "partner_id": partner.id,
+                "state": "open",
+            }
+        )
+
+        wizard = (
+            self.env["hr.cae.event.to.timesheet.wizard"]
+            .with_context({"active_id": event.id})
+            .create({"account_id": account.id, "project_id": project.id})
+        )
+
+        self.assertFalse(partner_user in wizard.user_ids)

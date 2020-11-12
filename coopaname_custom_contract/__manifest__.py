f0f30937f9c8ad0f3e6929497bac7cafe794bbba
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-11-12 17:36:14 +0100

    [ADD] coopaname_custom_contract: scaffolding

---
+++ coopaname_custom_contract/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Manuel Claeys Bouuaert <manuel@coopiteasy.be>
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Coopaname Custom Contract",
+    "summary": """
+        Customization of Contracts specific to Coopaname use case.
+    """,
+    "license": "AGPL-3",
+    "author": "Coop IT Easy SCRLfs",
+    "website": "https://www.coopiteasy.be",
+    "category": "Human Resources",
+    "version": "12.0.1.0.0",
+    "depends": ["coopaname_custom"],
+    "data": ["report/hr_contract_cape.xml"],
+    "demo": [],
+    "external_dependencies": {"python": ["phonenumbers"]},
+    "installable": True,
+}
---
+++ coopaname_custom_contract/models/hr_contract.py
@@ -0,0 +1,20 @@
+from odoo import api, models
+
+
+class Contract(models.Model):
+    _inherit = "hr.contract"
+
+    @api.multi
+    def create_document(self):
+        self.ensure_one()
+        if self.type_id == self.env.ref(
+            "hr_cae_contract.hr_contract_type_cape"
+        ):
+            action = self.env.ref(
+                "coopaname_custom_contract.action_hr_contract_cape"
+            ).report_action(self)
+        else:
+            action = self.env.ref(
+                "hr_cae_contract.action_hr_contract"
+            ).report_action(self)
+        return action
